d9545db9dae9c134c235ba07bceaa82cdff02cc6
Lazytree123 <luca.mihai.creator@gmail.com>
2021-08-29 17:59:13 +0200

    first version

---
+++ js.js
@@ -0,0 +1,12 @@
+function sub() {
+    alert("Tnx PS: this doesnt sub u " )
+}
+function mafv() {
+    alert("You Went To My actual first Video! ")
+}
+function pvp() {
+    alert("You went To Just a pvp Video!")
+}
+function ls2000() {
+   alert("You went to LazyTree Shorts!")
+}
